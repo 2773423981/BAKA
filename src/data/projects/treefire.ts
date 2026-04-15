44a13995b07fa7050400cd266f309867a6647a61
2773423981 <jjjkkklll258000@163.com>
2026-04-16 01:34:30 +0800

    modified:   src/components/features/projects/ProjectCard.astro 	modified:   src/content/spec/about/jinxing.md 	modified:   src/content/spec/about/treefire.md 	modified:   src/data/projects/treefire.ts

--- src/data/projects/treefire.ts
+++ src/data/projects/treefire.ts
@@ -1,3 +1,19 @@
 import type { Project } from "../../data/projects.ts";
 
-export const projectsData: Project[] = [];
+export const projectsData: Project[] = [
+	{
+		id: "game",
+		title: "拼图游戏",
+		description:
+			"虽然是一个toy级的项目，但通过询问AI完成了界面搭建、对接口信息的了解，我从底层重构了拼图块移动逻辑，大大减少了代码量以及增加了扩展性，本人也亲自完成自动提示/自动完成的算法。",
+		image: "",
+		category: "mobile",
+		techStack: ["Java", "CPP", "CMake"],
+		status: "completed",
+		sourceCode: "https://github.com/nonamecpp/PuzzleDemo001",
+		startDate: "2026-04-08",
+		endDate: "2026-04-15",
+		featured: true,
+		tags: ["Java", "CPP", "移动端游戏"],
+	},
+];
